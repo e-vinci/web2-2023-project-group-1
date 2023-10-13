ccd990827bac444e88fdce5e7d84cd69f91d0872
LotfiM1998 <113674279+LotfiM1998@users.noreply.github.com>
2023-10-13 13:11:32 +0200

    fix: returned  to request

--- api/routes/sitesPassword.js
+++ api/routes/sitesPassword.js
@@ -4,7 +4,7 @@ const { addPasswordOnSite } = require('../models/sites');
 const router = express();
 
 /* POST add 1 site to my db */
-router.post('/addSite', (req, res) => {
+router.post('/addSite', async (req, res) => {
   const usersId = parseInt(req.body.userId, 10);
   const {
     urlSite, siteName, userNameSite, passwordSite,
@@ -12,7 +12,7 @@ router.post('/addSite', (req, res) => {
   if (!siteName || !userNameSite || !passwordSite || usersId < 0) {
     return res.sendStatus(400);
   }
-  const returned = addPasswordOnSite(usersId, urlSite, siteName, userNameSite, passwordSite);
+  const returned = await addPasswordOnSite(usersId, urlSite, siteName, userNameSite, passwordSite);
   return res.json(returned);
 });
 
