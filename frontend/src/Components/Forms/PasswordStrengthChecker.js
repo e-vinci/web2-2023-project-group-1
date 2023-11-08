c40594c02fcf4dc29806c94f674e227b83d0d1a5
IweZix <IweZix@users.noreply.github.com>
2023-11-08 20:17:37 +0100

    refactor: passwordStrengthChecker style

--- frontend/src/Components/Forms/PasswordStrengthChecker.js
+++ frontend/src/Components/Forms/PasswordStrengthChecker.js
@@ -6,7 +6,7 @@ const { passwordStrength } = require('check-password-strength');
 const checkerForm = `
 <h1 class="text-center m-5">Vérificateur de mot de passe</h1>
 <p class="text-center" >Vous pensez que votre mot de passe est sécurisé ? Vérifier le içi.</p>
-<section class="d-flex justify-content-center border border-primary p-5">
+<section class="d-flex justify-content-center p-5">
   <form id="checker-form" style="text-align: left;">
     <div bis_skin_checked="1">
       <div class=" pt-1 pb-1" bis_skin_checked="1">
@@ -41,45 +41,35 @@ const listenersPasswordStrengthChecker = () => {
 
         const passwordStrengthResult = passwordStrength(passwordToCheck);
 
-        let val = passwordStrengthResult.value;
+        const val = passwordStrengthResult.value;
+        let resultHTML;
         if (val === 'Too weak') {
-            val = 'Trop faible';
+            resultHTML = `<p style="color: red;">Trop faible</p>`;
         } else if (val === 'Weak') {
-            val = 'Faible';
+            resultHTML = `<p style="color: orange;">Faible</p>`;
         } else if (val === 'Medium') {
-            val = 'Moyen';
+            resultHTML = `<p style="color: yellow;">Moyen</p>`;
         } else if (val === 'Strong') {
-            val = 'Fort';
-        }
-        const resultHTML = `
-          <p>${val}</p>
-        `;        
+            resultHTML = `<p style="color: green;">Fort</p>`;
+        }      
         checkerResult.innerHTML = 'La puissance de votre mot de passe : ';
         checkerResult.innerHTML += resultHTML;
 
         checkerAdvice.innerHTML = 'Nos conseil pour améliorer votre mot de passe : ';
         const advices = passwordStrengthResult.contains;
-        const advicesList = [];
-        if (!advices.includes('uppercase')) {
-          advicesList.push('Ajouter des majuscules');
+        const advicesList = advicesToArray(advices, passwordToCheck);
+        
+        if (advicesList.length === 0) {
+          checkerAdvice.innerHTML = 'Votre mot de passe est sécurisé';
+        } else {
+          const advicesHTML = `
+          <ul>
+            ${advicesList.map(advice => `<li>${advice}</li>`).join('')}
+          </ul>
+          `;
+          checkerAdvice.innerHTML += advicesHTML;
         }
-        if (!advices.includes('lowercase')) {
-          advicesList.push('Ajouter des minuscules');
-        }
-        if (!advices.includes('number')) {
-          advicesList.push('Ajouter des chiffres');
-        }
-        if (!advices.includes('symbol')) {
-          advicesList.push('Ajouter des caractères spéciaux');
-        }
-        if (passwordToCheck.length < 8) {
-          advicesList.push('Ajouter des caractères');
-        }
-        advicesList.forEach(e => {
-            checkerAdvice.innerHTML +=  `<p>${e}</p>`;
-        });
 
-        // front to back
         const option = {
             method: 'POST',
             body: JSON.stringify({
@@ -89,13 +79,38 @@ const listenersPasswordStrengthChecker = () => {
                 'Content-Type': 'application/json'
             }
         }
-        // ajouter await
         const response = fetch('/api/leaderboard/addLeaderboard', option);
         if (!response.ok) {
             console.log('Une erreur est survenue');
         }
     });
     
+}
+
+/**
+ * create a list of advice to improve the password
+ * @param {Array} array list of different type of character in the password
+ * @param {*} password the password to check
+ * @returns {Array} list of advice to improve the password
+ */
+function advicesToArray(array, password) {
+  const advicesList = [];
+  if (!array.includes('uppercase')) {
+    advicesList.push('Ajouter des majuscules');
+  }
+  if (!array.includes('lowercase')) {
+    advicesList.push('Ajouter des minuscules');
+  }
+  if (!array.includes('number')) {
+    advicesList.push('Ajouter des chiffres');
+  }
+  if (!array.includes('symbol')) {
+    advicesList.push('Ajouter des caractères spéciaux');
+  }
+  if (password.length < 8) {
+    advicesList.push('Ajouter des caractères');
+  }
+  return advicesList;
 }
 
 const renderPasswordStrengthChecker = () => {
