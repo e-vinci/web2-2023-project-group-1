acfa0f7fc424e4220bd44710c9bbc1908a2e7529
Cwen28 <94372138+Cwen28@users.noreply.github.com>
2023-11-09 17:50:48 +0100

    add : Frontend leaderboard

--- frontend/src/Components/Pages/CheckerLeaderboard.js
+++ frontend/src/Components/Pages/CheckerLeaderboard.js
@@ -3,13 +3,19 @@ import {
   renderPasswordStrengthChecker
 } from '../Forms/PasswordStrengthChecker';
 
+import {
+  leaderboardListener
+} from '../Leaderboard/leaderboard';
+
 const CheckerLeaderboard = () => {
   const main = document.querySelector('main');
   main.innerHTML = `
   <section class="checker" style="background-color: white; width: 60%; margin: auto; padding: 1%; border-radius: 5px; box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.75);"></section>
-  <section id="leaderboard"></section>
+  <section id="leaderboard" class="text-center"></section>
   `;
   renderPasswordStrengthChecker();
+  leaderboardListener();
+
 };
   
 export default CheckerLeaderboard;
