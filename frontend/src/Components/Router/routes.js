909a6a56ed3d2692d4e94d61968b0be19f77c502
LotfiM1998 <113674279+LotfiM1998@users.noreply.github.com>
2023-11-13 13:54:34 +0100

    add: intermediaire for crypto

--- frontend/src/Components/Router/routes.js
+++ frontend/src/Components/Router/routes.js
@@ -2,12 +2,14 @@ import HomePage from '../Pages/HomePage';
 import LoginRegister from '../Pages/LoginRegister';
 import GeneratePassword from '../Pages/GeneratePassword';
 import Logout from '../Logout/Logout';
+import UserPage from '../Pages/UserPage';
 
 const routes = {
   '/': HomePage,
   '/login': LoginRegister,
   '/generate': GeneratePassword,
   '/logout': Logout,
+  '/user':UserPage,
 };
 
 export default routes;
