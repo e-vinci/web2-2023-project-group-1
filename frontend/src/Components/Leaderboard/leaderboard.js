78796358f563fd16abbbff235d2d5176dd04f90a
Cwen28 <94372138+Cwen28@users.noreply.github.com>
2023-11-14 09:36:29 +0100

    commit : duplicatePassword

--- frontend/src/Components/Leaderboard/leaderboard.js
+++ frontend/src/Components/Leaderboard/leaderboard.js
@@ -19,6 +19,25 @@ const checkleaderboard = `
 </section>
 `;
 
+const checkDuplicatePassword = `
+<section class="w-75 p-3 mx-auto">
+  <table id="duplicatePassword" class="table table-striped table-hover">
+    <thead>
+        <tr class="border border-dark">
+            <th id="nomSite1">Nom du site 1 </th>
+            <th id="password1">Mots de passe</th>
+            <th id="nomSite2">Nom du site 2</th>
+            <th id="password2">Mots de passe</th>
+
+        </tr>
+    </thead>
+    <tbody id="duplicatePassword">
+    </tbody>
+  </table>
+</section>
+
+`;
+
 async function afficherMdp() {
     const reponse = await fetch(`/api/leaderboard/leaderboard`);
 
@@ -38,12 +57,41 @@ async function afficherMdp() {
     });
 }
 
+async function afficherDupliatePassword() {
+    const reponse = await fetch(`/api/leaderboard/leaderboard`);
+
+    const tableau = document.querySelector('#duplicatePassword');
+
+    const leaderboards = await reponse.json();
+
+    leaderboards.forEach((mdp, index) => {
+        const infoMdp = `
+        <tr>
+            <td>${index + 1}</td>
+            <td>${mdp.password}</td>
+            <td>${mdp.count}</td>
+            <td>${mdp.password}</td>
+
+        </tr>
+        `;
+        tableau.innerHTML += infoMdp;
+    });
+}
+
 const leaderboardListener = () => {
     const checker = document.querySelector('#leaderboard');
     checker.innerHTML = checkleaderboard;
     afficherMdp();
 }
 
+const duplicatepasswordListener = () => {
+    const checker = document.querySelector('#duplicatePassword');
+    checker.innerHTML = checkDuplicatePassword;
+    afficherDupliatePassword();
+
+}
+
 module.exports = {
-    leaderboardListener
+    leaderboardListener,
+    duplicatepasswordListener
 };
