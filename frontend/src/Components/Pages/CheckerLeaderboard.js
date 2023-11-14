ae7dbf0dc4c158f020a96e17089f94f2a7e6c46b
Cwen28 <94372138+Cwen28@users.noreply.github.com>
2023-11-14 09:36:29 +0100

    commit : duplicatePassword

--- frontend/src/Components/Pages/CheckerLeaderboard.js
+++ frontend/src/Components/Pages/CheckerLeaderboard.js
@@ -4,17 +4,23 @@ import {
 } from '../Forms/PasswordStrengthChecker';
 
 import {
-  leaderboardListener
+  leaderboardListener,
+  duplicatepasswordListener
 } from '../Leaderboard/leaderboard';
+
 
 const CheckerLeaderboard = () => {
   const main = document.querySelector('main');
   main.innerHTML = `
   <section class="checker" style="background-color: white; width: 60%; margin: auto; padding: 1%; border-radius: 5px; box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.75);"></section>
   <section id="leaderboard" class="text-center"></section>
+  <section id="duplicatePassword" class="text-center"></section>
   `;
   renderPasswordStrengthChecker();
   leaderboardListener();
+  duplicatepasswordListener();
+
+
 
 };
   
