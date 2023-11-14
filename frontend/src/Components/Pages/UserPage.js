9e80138c28c985d38ae521486732d0556b78936e
LotfiM1998 <113674279+LotfiM1998@users.noreply.github.com>
2023-11-14 09:30:11 +0100

    add: sidebar with click button

--- frontend/src/Components/Pages/UserPage.js
+++ frontend/src/Components/Pages/UserPage.js
@@ -1,34 +1,63 @@
-const UserPage = () => {
-
-  const listSite = async => {
-    const option = {
-      method: 'GET',
-      body: JSON.stringify({
-        "login": pseudo,
-        "email": email,
-        "password": password
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }
-
-    const response =  fetch('/api/auths/orderBySiteName', option);
+const defaultList= [
+  {
+      "id": 1,
+      "url": "test01",
+      "site": "testSite1",
+      "login": "Useraname1",
+      "mot_de_passe": "$2b$10$fe2S0.EVIParWe1jAoagE.oxCfWS95JUX0YtG.mOnHdEB5zfi4TUW"
+  },
+  {
+      "id": 2,
+      "url": "test01",
+      "site": "testSite1",
+      "login": "Useraname1",
+      "mot_de_passe": "$2b$10$0rQmPNWcskf5Ce2/hKYRFu0mDELZpc4VSN406e3rogrIo6/165362"
+  },
+  {
+      "id": 3,
+      "url": "test01",
+      "site": "testSite1",
+      "login": "Useraname1",
+      "mot_de_passe": "$2b$10$cWHPZkMaeGY2PSCXPHk6NOGDdsHipiSKCj0kYe9lC5Z/roMYDea6K"
+  },
+  {
+      "id": 4,
+      "url": "test01",
+      "site": "testSite1",
+      "login": "Useraname1",
+      "mot_de_passe": "$2b$10$BrUjYe2DNaRXF3oRj0oZxu/MkfoBmBphPaEMTZ9HPYFsjXLkdmMbC"
   }
+];
 
+const sidebarToFill=`<!--Main Navigation-->
+  <header>
+  <div class="container">
+  <div class="row">
+    <div class="listSite">
+    </div>
+    <div class="col-sm">
+      The other box
+    </div>
+    </div>
+</div>`;
+
+const UserPage = () => {
   const main = document.querySelector('main');
-  const divRow = document.createElement('row');
-  const divTable = document.createElement('table');
+  main.className = 'd-flex align-items-center justify-content-center';
+  main.innerHTML = sidebarToFill;
+  const sideBar=document.querySelector('.listSite');
+  defaultList.forEach((element)=>{
+    const listelem=document.createElement('li');
+    const elem=document.createElement('button');
+    elem.id = element.id;
+    elem.innerHTML=element.site;
+    listelem.appendChild(elem);
+    sideBar.appendChild(listelem);
+  }
+   );
+  
 
-  divTable.className = 'table table-dark table-sm';
-  divTable.innerHTML = 'Vos comptes';
-  for ()
 
-
-    divRow.className = 'container-fluid';
-  divRow.innerHTML = 'Ma colonne';
-  divRow.appendChild(divTable);
-  main.appendChild(divRow);
 };
 
 export default UserPage;
