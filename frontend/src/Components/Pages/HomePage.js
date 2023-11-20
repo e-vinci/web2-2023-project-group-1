c24ab7a03c1e2875a14d256abe1a5f0fbcebdb89
Cwen28 <94372138+Cwen28@users.noreply.github.com>
2023-11-20 14:13:41 +0100

    refactor : design leaderboard

--- frontend/src/Components/Pages/HomePage.js
+++ frontend/src/Components/Pages/HomePage.js
@@ -6,7 +6,7 @@ const HomePage = () => {
   main.className = 'align-items-center justify-content-center';
   main.innerHTML = `
   <section class="checker" style="background-color: white; width: 60%; margin: auto; padding: 1%; border-radius: 5px; box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.75);"></section>
-  <section id="leaderboard" class="text-center"></section>
+  <section id="leaderboard" style ="padding: 2%" class="text-center"></section>
   `;
   renderPasswordStrengthChecker();
   leaderboardListener();
