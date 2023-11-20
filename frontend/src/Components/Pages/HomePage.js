56be468552ac989d9e60b16f72e900524e09fc66
Cwen28 <94372138+Cwen28@users.noreply.github.com>
2023-11-20 14:30:42 +0100

    resetting history

--- frontend/src/Components/Pages/HomePage.js
+++ frontend/src/Components/Pages/HomePage.js
@@ -8,6 +8,7 @@ const HomePage = () => {
   <section class="checker" style="background-color: white; width: 60%; margin: auto; padding: 1%; border-radius: 5px; box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.75);"></section>
   <section id="leaderboard" class="text-center"></section>
   `;
+  
   renderPasswordStrengthChecker();
   leaderboardListener();
   animationTitre();
