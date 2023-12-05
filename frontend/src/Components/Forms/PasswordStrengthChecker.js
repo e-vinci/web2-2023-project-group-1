7f93eb2fd5c7aca8a9dd650f331029559be8395f
Cwen28 <94372138+Cwen28@users.noreply.github.com>
2023-12-05 10:07:14 +0100

    refactor : animationCheckPassword

--- frontend/src/Components/Forms/PasswordStrengthChecker.js
+++ frontend/src/Components/Forms/PasswordStrengthChecker.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-alert */
 /* eslint-disable import/no-import-module-exports */
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable import/no-unresolved */
