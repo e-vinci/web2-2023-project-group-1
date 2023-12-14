cb2eadc68b993564596169771865a6b592667903
Cwen28 <94372138+Cwen28@users.noreply.github.com>
2023-12-14 17:37:48 +0100

    add : modal GDPR

--- frontend/src/Components/passwordGenerator/generator.js
+++ frontend/src/Components/passwordGenerator/generator.js
@@ -19,7 +19,7 @@ const modalGenerator = `
               <div class="modal-body w-100" id="inner-modal">
               </div>
               <div class="modal-footer">
-                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
               </div>
           </div>
       </div>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './stylesheets/main.css';
 import { renderGeneratorPassword } from './Components/passwordGenerator/generator';
 import { renderAbout } from './Components/About/about';
+import { renderGDPR } from './Components/GDPR/GDPR';
 
 import Navbar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/footer';
 import Router from './Components/Router/Router';
 
+renderGDPR();
 renderAbout();
 renderGeneratorPassword();
 Navbar();
